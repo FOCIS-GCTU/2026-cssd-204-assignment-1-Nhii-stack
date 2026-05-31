9b78cae40cfd0c59668559d0727fd3b6d5d32f29
Philemon <2425403698@live.gctu.edu.gh>
2026-05-31 01:00:38 +0000

    Add files via upload
    Scripting Languages 1

---
+++ initial.py
@@ -0,0 +1,33 @@
+# File: initials.py
+# Description: Print out my initials
+# Assignment Number: 1
+#
+# Name: Philemon Quarshie
+# STUDENT ID: 2425403698
+# Email: 2425403698@live.gctu.edu.gh
+# Grader: Augustus
+#
+#On my honor, Philemon Quarshie, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+def main():
+    # Print my initials in large stylized letters.
+
+    print()
+    print("...PQN")
+    print()
+
+    print("...PPPPPPPPPPPP...QQQQQQQQQQQQ...NNN.......NN...")
+    print("...PPP......PPP...QQQ......QQQ...NNN.......NN...")
+    print("...PPP......PPP...QQQ......QQQ...NNNN......NN...")
+    print("...PPP......PPP...QQQ......QQQ...NN.NN.....NN...")
+    print("...PPPPPPPPPPPP...QQQ......QQQ...NN..NN....NN...")
+    print("...PPP............QQQ......QQQ...NN...NN...NN...")
+    print("...PPP............QQQ......QQQ...NN....NN..NN...")
+    print("...PPP............QQQ....QQ.QQ...NN....NNN.NN...")
+    print("...PPP.........  ...QQQQQQQQQQ  ...NN......NNNN...")
+    print("...PPP.........  ..........QQ.  ...NN......NNNN...")
+    print()
+
+
+    main()
